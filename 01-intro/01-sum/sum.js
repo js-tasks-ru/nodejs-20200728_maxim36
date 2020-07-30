71bc99e082517a87f8ff4d407aae3d1c8e73a8d0
Maxim <kokovin.maxim@yandex.ru>
2020-07-30 13:13:12 +0800

    remove tab

--- 01-intro/01-sum/sum.js
+++ 01-intro/01-sum/sum.js
@@ -1,6 +1,6 @@
 function sum(a, b) {
-	const typeA = typeof a;
-	const typeB =typeof b;
+  const typeA = typeof a;
+  const typeB = typeof b;
   if ( typeA !== 'number' || typeB !== 'number') return new TypeError('Not a number');
   else return a + b;
 }
