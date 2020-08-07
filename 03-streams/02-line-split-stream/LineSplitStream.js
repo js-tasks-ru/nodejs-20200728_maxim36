65d5d4aa646bd172b6d20e9abea34011e55fc4d3
Maxim <kokovin.maxim@yandex.ru>
2020-08-07 21:15:45 +0800

    second task has been resolved

--- 03-streams/02-line-split-stream/LineSplitStream.js
+++ 03-streams/02-line-split-stream/LineSplitStream.js
@@ -3,13 +3,35 @@ const os = require('os');
 
 class LineSplitStream extends stream.Transform {
   constructor(options) {
-    super(options);
+    const {encoding, other} = options;
+    super(other);
+    this._buffer = '';
+    this.setEncoding(encoding);
   }
 
   _transform(chunk, encoding, callback) {
+    console.log(chunk.toString());
+    chunk.toString().split(os.EOL).forEach((item, i, arr) => {
+      if (arr.length === 1) {
+        this._buffer += item;
+        return;
+      };
+      if (i === 0) {
+        this.push(this._buffer.concat(item));
+        this._buffer = '';
+        return;
+      };
+      if (i === arr.length - 1) {
+        this._buffer = item;
+        return;
+      }
+      this.push(item);
+    });
+    callback();
   }
 
   _flush(callback) {
+    callback(null, this._buffer);
   }
 }
 
