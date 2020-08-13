356434af06beab91adb7278d9a27a98e3d69b169
Maxim <kokovin.maxim@yandex.ru>
2020-08-13 08:52:53 +0800

    Revert "task 05 http-server-srteams"
    This reverts commit 12c9e5b82ba8059b96d2a09ea061295649b892b5.
    
    Revert previous commit

--- 05-http-server-streams/03-file-server-delete/server.js
+++ 05-http-server-streams/03-file-server-delete/server.js
@@ -1,58 +1,23 @@
 const url = require('url');
 const http = require('http');
 const path = require('path');
-const fs = require('fs');
 
 const server = new http.Server();
-
-const hasFile = (filepath) => {
-  return new Promise((resolve) => {
-    fs.access(filepath, (err) => {
-      if (err) resolve(false);
-      else resolve(true);
-    })
-  })
-}
-
-const handlers = {
-  async DELETE(req, res, filepath) {
-    const fileExist = await hasFile(filepath);
-    if (!fileExist) {
-      res.statusCode = 404;
-      res.end('Not found');
-      return;
-    };
-    fs.unlink(filepath, (err) => {
-      if (err) {
-        res.statusCode = 500;
-        res.end();
-        return;
-      }
-      res.statusCode = 200;
-      res.end();
-    })
-  }
-}
 
 server.on('request', (req, res) => {
   const pathname = url.parse(req.url).pathname.slice(1);
 
-  if (pathname.indexOf('/') !== -1) {
-    res.statusCode = 400;
-    res.end('Bad request');
-    return;
-  };
+  const filepath = path.join(__dirname, 'files', pathname);
 
-  const filepath = path.join(__dirname, 'files', pathname).replace(/`path.delimetr`/g, '/');
+  switch (req.method) {
+    case 'DELETE':
 
-  const handler = handlers[req.method];
-  if (handler) {
-    handler(req, res, filepath);
-  } else {
-    res.statusCode = 501;
-    res.end('Not implemented');
+      break;
+
+    default:
+      res.statusCode = 501;
+      res.end('Not implemented');
   }
-})
-
+});
 
 module.exports = server;
