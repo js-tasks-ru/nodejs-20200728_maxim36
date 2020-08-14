117b628951a438d3761ad851c6d1a1483d085448
Maxim <kokovin.maxim@yandex.ru>
2020-08-14 22:37:48 +0800

    06-koajs 01-chat-app

--- 06-koajs/01-chat-app/app.js
+++ 06-koajs/01-chat-app/app.js
@@ -7,11 +7,39 @@ app.use(require('koa-bodyparser')());
 
 const Router = require('koa-router');
 const router = new Router();
+const subscribes = new Set();
+
+const waiter = (ctx) => {
+    let commonResolve = null;
+
+    const starter = (message) => {
+        if (!message) ctx.body = ''
+        else ctx.body = message;
+        commonResolve();
+    }
+
+    const throttel = () => {
+        return new Promise( (resolve) => {
+            commonResolve = resolve;
+        })
+    }
+    return [starter, throttel];
+}
 
 router.get('/subscribe', async (ctx, next) => {
+    ctx.response.status = 200;
+    const [starter, throttel] = waiter(ctx);
+    subscribes.add(starter);
+    await throttel();
 });
 
 router.post('/publish', async (ctx, next) => {
+    const message = ctx.request.body;
+    for (const subscribe of subscribes) {
+        subscribe(message);
+        subscribes.delete(subscribe);
+    }
+    ctx.response.status = 201;
 });
 
 app.use(router.routes());
